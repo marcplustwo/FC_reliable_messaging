132893149f717351762d7f74ed973a3c5e6011f6
Tianle <tianleluo@foxmail.com>
2022-06-20 17:12:06 +0200

    add functions

--- app/edge/sensor/sensor.py
+++ app/edge/sensor/sensor.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import random
 
 class Sensor:
@@ -15,3 +16,20 @@ class Sensor:
     #pass the generated temp to which queue?
     
     return 0
+
+#kkkk
+
+=======
+from typing import List
+from random import random
+
+
+class Sensor:
+    def __init__(self, range: List[int]):
+        # initialize data range
+        self.range = range
+
+    def read(self) -> int:
+        # random data in range
+        return random() * (self.range[1] - self.range[0]) + self.range[0]
+>>>>>>> 538b7e818d50c96e587b9bb56fd572aa958b16c6
--- app/server/server.py
+++ app/server/server.py
@@ -1,8 +1,17 @@
+import zmq
+
 # init message receiver
-#
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+socket.bind("tcp://*:5555")
+
 # loop:
 #   recv data
 #   store data
 #
 #   (not every iteration but regularly e.g. timer):
 #     aggregate data and send out via message queue
+
+while True:
+  msg = socket.recv()
+  print(f"receeived: {msg}")
