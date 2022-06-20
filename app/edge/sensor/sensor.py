56bdf2d2e72af18ba5f47076e7c706c27831f1fb
Tianle <tianleluo@foxmail.com>
2022-06-20 13:09:40 +0200

    method generating sensor random data

--- app/edge/sensor/sensor.py
+++ app/edge/sensor/sensor.py
@@ -1,9 +1,17 @@
+import random
+
 class Sensor:
   def __init__(self):
     # initialize data range
+    minTemp = random.uniform(0,15)  #unit °C
+    maxTemp = random.uniform(20,40)
     # how to generate data
+    temp = random.uniform(minTemp,maxTemp)
     pass
 
   def read(self):
     # random data
+    
+    #pass the generated temp to which queue?
+    
     return 0
